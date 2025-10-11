69bf53e6b0dcf2777f786471d9b38a8fb2a84858
alexchtst-01 <acinatra@gmail.com>
2025-10-11 21:53:12 +0700

    feat: edit developer profile

--- src/screen/landing.tsx
+++ src/screen/landing.tsx
@@ -6,10 +6,10 @@ export default function Landing() {
         <MainLayout needProtection={false}>
             <div className="w-full h-full flex items-center justify-center my-10">
                 <div className="w-fit space-y-5 md:flex md:items-center md:justify-evenly md:space-y-0 md:w-full">
-                    <DeveloperCard Photo="/alex.jpeg" NamaDeveloper="Alex Cinatra" Role="Beban Tim" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/alex-cinatra-520023256/"/>
-                    <DeveloperCard Photo="/ryan.jpg" NamaDeveloper="Ryan Krishandi Lukito" Role="Frontend Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/ryan-krishandi-lukito/"/>
-                    <DeveloperCard Photo="/arden.jpg" NamaDeveloper="Cornelius Arden S.H." Role="Backend Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/ardenhermawan/"/>
-                    <DeveloperCard Photo="/varick.jpg" NamaDeveloper="Varick Zahir Sarjiman" Role="Smart Contract Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/varick-zahir-sarjiman/"/>
+                    <DeveloperCard Photo="/alex-profile.jpg" NamaDeveloper="Alex Cinatra" Role="Project Leader" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/alex-cinatra-520023256/"/>
+                    <DeveloperCard Photo="/ryan-profile.jpg" NamaDeveloper="Ryan Krishandi Lukito" Role="Frontend Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/ryan-krishandi-lukito/"/>
+                    <DeveloperCard Photo="/arden-profile.jpeg" NamaDeveloper="Cornelius Arden S.H." Role="Backend Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/ardenhermawan/"/>
+                    <DeveloperCard Photo="/varick-profile.jpg" NamaDeveloper="Varick Zahir Sarjiman" Role="Smart Contract Developer" Deskripsi="Deskripsi" Linkedin="https://www.linkedin.com/in/varick-zahir-sarjiman/"/>
                 </div>
             </div>
         </MainLayout>
